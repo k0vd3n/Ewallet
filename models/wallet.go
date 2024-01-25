bc8e6cae03f4aaef9a1e868606d5583ec7cd5f77
User <dborodenkov1@gmail.com>
2024-01-25 20:57:33 +0300

    edit models/wallet.go

--- models/wallet.go
+++ models/wallet.go
@@ -11,8 +11,8 @@ type Wallet struct {
 // Transaction модель для транзакции
 type Transaction struct {
 	ID         string    `gorm:"type:varchar(32);primary_key" json:"id"`
-	WalletID   string    `gorm:"type:varchar(32);references:wallets;column:id" json:"wallet_id"`
-	ToWalletID string    `gorm:"type:varchar(32);references:wallets;column:id" json:"to_wallet_id"`
+	WalletID   string    `gorm:"type:varchar(32);references:wallets;column:wallet_id" json:"wallet_id"`
+	ToWalletID string    `gorm:"type:varchar(32);references:wallets;column:to_wallet_id" json:"to_wallet_id"`
 	Amount     float64   `gorm:"type:decimal(10,2)" json:"amount"`
 	Time       time.Time `gorm:"type:timestamp without time zone;default:current_timestamp" json:"time"`
 }
