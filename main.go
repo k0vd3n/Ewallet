7a1de87dd4121a39e058eda42a81492d130c3687
User <dborodenkov1@gmail.com>
2024-01-25 20:44:26 +0300

    edit main.go file

--- main.go
+++ main.go
@@ -1,10 +1,18 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"Ewallet/api"
+	"Ewallet/db"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	// Инициализация маршрутов и подключение к базе данных
 	r := gin.Default()
+	db.InitDB()
+	api.InitRoutes(r)
+
 	// Запуск сервера
 	r.Run(":8080")
 }
